fafedda7186139426bed6243b09285fd46a91527
Gabriel Sosa <gaby_s.g@outlook.es>
2023-09-13 21:35:16 -0300

    agregando if y else

--- JS/main.js
+++ JS/main.js
@@ -10,20 +10,26 @@ function convertirPesos() {
         while (isNaN(cantidad)) {
             cantidad = prompt("¡Ingrese un número válido!");
             console.log("No es valido"); // suponiendo que usen letras repetidas veces
+            console.log("----------------")
         }
 
         let dolares = cantidad / 700; // Suponiendo que 700 pesos equivale a 1 dolar
 
         console.log(cantidad + " pesos equivale a " + dolares + " dólares.");
 
-        opcion = prompt("Ingrese una opción: 1 para convertir otra vez, 2 para salir.");
-        console.log("----------------")
+        opcion = prompt("Ingrese una opción: 1 para otra conversion, 2 para salir.");
+
+        if (opcion === "2") {
+            console.log("Saliendo del programa..."); 
+            console.log("Conversor de moneda finalizó."); 
+        } else if (opcion !== "1") {
+            console.log("Opción no válida. Saliendo del programa...");
+            console.log("Conversor de moneda finalizó.");
+        }
+        
+        console.log("----------------");
     }
-
-    console.log("Saliendo del programa...");
-    console.log("Conversor de moneda finalizo");
 }
-
 
 convertirPesos();
 
